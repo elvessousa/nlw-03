72f1f4cb8c0e9f8fc30d55f1ac8b62c2ccb2954e
Elves Sousa <elves.sousa@gmail.com>
2020-10-12 16:38:56 -0300

    First commit.

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import { FiArrowDownRight } from "react-icons/fi";
+
+import '../styles/pages/landing.css';
+import logoImg from '../images/logo.svg';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo"/>
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowDownRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Landing;
