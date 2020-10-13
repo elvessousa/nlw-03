46e9647a7db441a041238f01e6479fbe228fe16b
Elves Sousa <elves.sousa@gmail.com>
2020-10-13 18:17:42 -0300

    Backend api.

---
+++ backend/src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import OrphanagesController from './controllers/OrphanagesController';
+import multer from 'multer';
+
+import uploadConfig from './config/uploads';
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+
+export default routes;
